70e14f8dd0ab3db730253122edf3e19ab90dd6f3
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-09-28 17:19:39 +0900

    Update PaymentOptionCard.vue

--- frontend/src/components/PaymentOptionCard.vue
+++ frontend/src/components/PaymentOptionCard.vue
@@ -63,7 +63,7 @@ watch(
     if (icons && icons.length > 1) {
       rotationTimer = setInterval(() => {
         activeIconIndex.value = (activeIconIndex.value + 1) % icons.length
-      }, 4000)
+      }, 3000)
     }
   },
   { immediate: true }
