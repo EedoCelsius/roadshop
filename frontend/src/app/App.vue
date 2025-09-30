22c59107370eac33ba96039d2262e72f08c7a887
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-01 04:35:24 +0900

    Update App.vue

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -7,7 +7,7 @@ import Experience from '@/payments/components/Experience.vue'
 <template>
   <div class="flex min-h-screen flex-col bg-gradient-to-b from-white via-roadshop-highlight to-white">
     <HeroSection />
-    <main class="mx-auto mt-16 w-full max-w-5xl flex-1 px-6 pb-24">
+    <main class="mx-auto mt-8 w-full max-w-5xl flex-1 px-6 pb-8">
       <Experience />
     </main>
     <FooterSection />
