a0ec5cf0396ba3588e23b0d24293fa7cf3277766
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-10-04 02:55:22 +0900

    Revert "Revert "Add router support for GitHub Pages dialog routes""

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Experience from '@/payments/components/Experience.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col bg-brand-highlight/40">
     <main class="mx-auto w-full max-w-5xl flex-1 px-6 py-12">
-      <Experience />
+      <RouterView />
     </main>
   </div>
 </template>
